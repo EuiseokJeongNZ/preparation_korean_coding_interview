e090be588383cb903dd957e4913342504f980577
EuiseokJeongNZ <153467430+EuiseokJeongNZ@users.noreply.github.com>
2026-01-05 16:13:42 +1300

    [level 2] Title: 게임 맵 최단거리, Time: 15.49 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,30 @@
 from collections import deque
 
 def solution(maps):
-    def bfs(i, j):
-        q = deque([(i, j)])
-        visited[i][j] = True
+    def bfs(s_i, s_j, n, m, visited, maps):
+        q = deque([])
+        q.append((s_i, s_j))
+        visited[s_i][s_j] = True
         
         while q:
             i, j = q.popleft()
-            
             for d in range(4):
                 ni, nj = i+di[d], j+dj[d]
-                if 0<=ni<len(maps) and 0<=nj<len(maps[0]):
+                if 0<=ni<n and 0<=nj<m:
                     if maps[ni][nj] == 1 and not visited[ni][nj]:
                         maps[ni][nj] += maps[i][j]
                         q.append((ni, nj))
-                        visited[ni][nj] = True
-        return
+                        visited[ni][nj]
     
+    n = len(maps)
+    m = len(maps[0])
+    visited = [[False]*(m+1) for _ in range(n)]
     di = [-1, 1, 0, 0]
     dj = [0, 0, -1, 1]
-    visited = [[False]*(len(maps[0])) for _ in range(len(maps))]
-        
-    bfs(0, 0)
     
-    if maps[len(maps)-1][len(maps[0])-1] == 1:
+    bfs(0, 0, n, m, visited, maps)
+    
+    if maps[n-1][m-1] == 1:
         return -1
-    
-    return maps[len(maps)-1][len(maps[0])-1]
+
+    return maps[n-1][m-1]
